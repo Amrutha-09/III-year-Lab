60b25c1e418c8b6f6672e9a4b5aa86ebf069bc66
Amrutha Chittampally <68068842+Amrutha-09@users.noreply.github.com>
2021-06-11 14:53:16 +0530

    Add files via upload

---
+++ CN&WT LAB/WT Experiments/EXPERIMENT 2/exp2.php
@@ -0,0 +1,21 @@
+1.	
+<?php 
+    $str = "Hello"; 
+    echo strlen($str); 
+?>  
+2.
+<?php
+echo str_word_count("How aRe you?");
+?>
+
+3.
+<?php
+echo strrev("Reverse me");
+?>
+
+4.
+<?php
+if (strpos('A b c d', 'b')) {
+    echo "Checking the existence of the empty string will always return true";
+}
+?>
