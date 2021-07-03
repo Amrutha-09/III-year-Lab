36acd7ad7fb917e24c88ec0bb1a6e85d4aebbb2e
Amrutha Chittampally <68068842+Amrutha-09@users.noreply.github.com>
2021-07-03 18:21:40 +0530

    Add files via upload

---
+++ SL Lab/Experiment1/expe1a.rb
@@ -0,0 +1,9 @@
+def stringcopy(str,n)
+    return str*n
+end
+puts "The result is:"
+puts stringcopy('x',1)
+puts stringcopy('x',2)
+puts stringcopy('x',3)
+puts stringcopy('x',4)
+puts stringcopy('x',5)
